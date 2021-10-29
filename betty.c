61a7114f28cf56e78b67144819bf50e9418f4313
jmoseka <jamylam3@gmail.com>
2021-10-29 09:20:35 +0000

    Code that passes the betty checks

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: code that passes Betty checks
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("This code passes Betty checks:)\n");
+	return (0);
+}
