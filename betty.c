9da10af2fa2ebfb8e5d41133da0e4e8b78507050
jmoseka <61447440+jmoseka@users.noreply.github.com>
2021-11-09 11:11:47 +0300

    Create betty.c

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: code that passes Betty checks
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("This code passes the Betty checks!\n");
+	return (0);
+}
